141215e9ec94c13e5a77578167dfda91b3ab6329
mink-park <pmk3000@nate.com>
2023-07-21 03:56:39 +0900

    set _chatlogic to unique_ptr for task1

--- src/chatgui.h
+++ src/chatgui.h
@@ -2,6 +2,7 @@
 #define CHATGUI_H_
 
 #include <wx/wx.h>
+#include <memory>
 
 class ChatLogic; // forward declaration
 
@@ -15,8 +16,7 @@ private:
 
     //// STUDENT CODE
     ////
-
-    ChatLogic *_chatLogic;
+    std::unique_ptr<ChatLogic> _chatLogic;
 
     ////
     //// EOF STUDENT CODE
